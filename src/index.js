d01c217f375589bf8d004637649ec5be4efad722
Em <emily.clay.k@gmail.com>
2020-03-16 14:21:01 -0700

    adds additional text to panel on mouse enter, moves dom elements from js to html

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
-const body = document.querySelector('body');
+const panelElements = document.querySelectorAll('.panel');
 
-const galleryElement = document.createElement('div');
-galleryElement.classList.add('gallery');
-
-"Let's take it all in".split(' ').forEach(panelText => {
-  const panelElement = document.createElement('div');
-
-  const textElement = document.createElement('span');
-  textElement.textContent = panelText;
-  panelElement.appendChild(textElement);
-
-  panelElement.classList.add('panel');
-
+panelElements.forEach(panelElement => {
   panelElement.addEventListener('mouseenter', ({ target }) =>
     target.classList.add('panel-active')
   );
   panelElement.addEventListener('mouseleave', ({ target }) =>
     target.classList.remove('panel-active')
   );
-
-  galleryElement.appendChild(panelElement);
 });
-
-body.prepend(galleryElement);
